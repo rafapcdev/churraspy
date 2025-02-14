832c834dadc7b3e71d3297556dd7f5bfadc544f7
matheus costa <dev.matheuspc@gmail.com>
2025-02-14 19:47:36 -0300

    inserido o trabalho do leandro

--- app.py
+++ app.py
@@ -64,6 +64,10 @@ def calcular():
 
     return jsonify({"url": url_for("resultado")})
 
+@app.route("/desenvolvedores")
+def desenvolvedores():
+    return render_template("")
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # Cria as tabelas no banco de dados
