7c445be2871082c6106e4bf09f61a4dac0070e35
matheus costa <dev.matheuspc@gmail.com>
2025-02-14 19:55:16 -0300

    inserido html dinamico pra pagina desenvolvedores

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def calcular():
 
 @app.route("/desenvolvedores")
 def desenvolvedores():
-    return render_template("")
+    return render_template("desenvolvedores.html")
 
 if __name__ == "__main__":
     with app.app_context():
